c758deafd952d65d4c05aba27ce5e4c9b7fc1466
DenisAndrieiew <eretarmineret@gmail.com>
2021-03-23 18:53:35 +0200

    refactor zip method

--- src/main/java/Exercise.java
+++ src/main/java/Exercise.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,8 +8,8 @@ import java.util.stream.Stream;
 public class Exercise {
     static List<String> NAMES = Arrays.asList("Ivan", "Peter", "Henry", "Joan", "Michael", "Bob", "Poll", "ZZ Top");
     static String[] TESTING_ARRAY = {"1, 2, 0", "4, 5"};
-    static Stream FIRST_TEST_STREAM = Stream.of(2,5,7,9);
-    static Stream SECOND_TEST_STREAM= Stream.of(11,44,23,94,75,67,32);
+    static Stream FIRST_TEST_STREAM = Stream.of(2, 5, 7, 9);
+    static Stream SECOND_TEST_STREAM = Stream.of(11, 44, 23, 94, 75, 67, 32);
     static long SEAD = 23456987L;
 
     public static String ex1(List<String> list) {
@@ -39,19 +39,16 @@ public class Exercise {
     public static Stream<Long> ex4(long a, long c, long m, long sead) {
         return Stream.iterate(sead, n -> ((n * a + c) % m));
     }
-    public static<T> Stream<T> zip(Stream<T> first, Stream<T> second){
-List<T> firstList = first.collect(Collectors.toList());
-List<T> secondList = second.collect(Collectors.toList());
-List<T> newList = new LinkedList<>();
-int counter = 0;
-int size = Math.min(firstList.size(), secondList.size());
-while (counter<size) {
-    newList.add(firstList.get(counter));
-    newList.add(secondList.get(counter));
-    counter++;
-}
 
-        return newList.stream();
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        Iterator<T> iteratorFirst = first.iterator();
+        Iterator<T> iteratorSecond = second.iterator();
+        Stream<T> toReturn = Stream.ofNullable(null);
+        while (iteratorFirst.hasNext() && iteratorSecond.hasNext()) {
+            toReturn = Stream.concat
+                    (toReturn, Stream.of(iteratorFirst.next(), iteratorSecond.next()));
+        }
+        return toReturn;
     }
 
     public static void main(String[] args) {
